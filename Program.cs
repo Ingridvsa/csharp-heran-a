cc60e3af8d9d4f8758b92bf9194ab073f090be28
Ingrid <ingridaleixo59@gmail.com>
2024-01-22 01:45:33 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using _5.Rpg.Models;
+
+string nome = Console.ReadLine();
+int mana = int.Parse(Console.ReadLine());
+int danoBase = int.Parse(Console.ReadLine());
+      
+Dano meuPersonagem = new Dano(nome, mana, danoBase);
+      
+meuPersonagem.CalcularDano();
